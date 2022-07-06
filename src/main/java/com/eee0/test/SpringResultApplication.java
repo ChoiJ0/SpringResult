0d0b8dfc4091864eda9df7188d8b5f1e05ad6862
eee0 <eee.0@naver.com>
2022-07-06 21:47:31 +0900

    [최재헌] - 초기설정

---
+++ src/main/java/com/eee0/test/SpringResultApplication.java
@@ -0,0 +1,13 @@
+package com.eee0.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eee0/test/SpringResultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eee0.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
