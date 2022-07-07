19e6526c33c71b82e828619eb57f8edd7a202314
eee0 <eee.0@naver.com>
2022-07-07 19:02:32 +0900

    [최재헌] - spring 설정 추가

--- src/main/java/com/eee0/test/SpringResultApplication.java
+++ src/main/java/com/eee0/test/SpringResultApplication.java
@@ -1,8 +1,17 @@
 package com.eee0.test;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+@MapperScan(basePackages = "com.eee0.test.*")
 @SpringBootApplication
 public class SpringResultApplication {
 
@@ -10,4 +19,14 @@ public class SpringResultApplication {
 		SpringApplication.run(SpringResultApplication.class, args);
 	}
 
+	@Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
 }
